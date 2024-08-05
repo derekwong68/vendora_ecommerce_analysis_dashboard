31cbc8a469e863ecba03669558468110034e2747
Derek Wong <166466027+derekwong68@users.noreply.github.com>
2024-08-05 19:37:22 -0400

    Update vendora_dataload.sql

--- vendora_dataload.sql
+++ vendora_dataload.sql
@@ -1,10 +1,10 @@
---select all columns from elist datasets
+--select all columns from vendora datasets
 --clean product name and create time to ship and time to purchase colunmns
 select orders.id as order_id,
   orders.customer_id,
   orders.purchase_ts,
   orders.product_id,
-  case when lower(product_name) like '%gaming monitor%' then '2in" 4k Gaming Monitor'
+  case when lower(product_name) like '%gaming monitor%' then '27in" 4k Gaming Monitor'
     when lower(product_name) = 'bose soundsport headphones' then 'Bose Soundsport Headphones'
     else product_name end as product_name_clean,
   orders.currency,
@@ -22,10 +22,10 @@ select orders.id as order_id,
   order_status.refund_ts,
   date_diff(order_status.ship_ts, orders.purchase_ts, day) as time_to_ship_days,
   date_diff(order_status.purchase_ts, customers.created_on, day) as time_to_purchase_days
-from elist.orders
-left join elist.customers
+from `vendora-431118.vendora.orders` orders
+left join `vendora-431118.vendora.customers` customers
 	on orders.customer_id = customers.id
-left join elist.geo_lookup
+left join `vendora-431118.vendora.geo_lookup` geo_lookup
 	on geo_lookup.country = customers.country_code
-left join elist.order_status
+left join `vendora-431118.vendora.order_status` order_status
 	on order_status.order_id = orders.id
